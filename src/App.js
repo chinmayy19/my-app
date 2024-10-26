1efcf397afdb8ecc5ad9212f11111c1ad5ed52ef
chinmayy19 <chinmayrathod2003@gmail.com>
2024-10-26 20:32:46 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,39 @@ import Register from "./pages/register/Register";
 
 import {
   createBrowserRouter,
+  Outlet,
   RouterProvider,
 } from "react-router-dom";
 
 function App() {
 
+  const Layout  = ()=>{
+    return(
+      <div>
+        <Navbar/>
+        <div style={{display: "flex"}}>
+        <LeftBar/>
+        <Outlet/>
+        <RightBar/>
+        </div>
+      </div>
+    )
+      
+  }
+
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children: [
+        {
+          path: "/",
+          element: <Home/>,
+        },
+        {
+          path: "/profile/:id",
+        }
+    },
     {
       path: "/login",
       element: <Login/>,
